30816f365c49bd21e734e7d9cc48ba42ce9f0361
Brad <Brad.nguyen2512@gmail.com>
2023-03-12 17:39:37 -0400

    Automation Project

--- Automation Project/YoutubeDownloader.py
+++ Automation Project/YoutubeDownloader.py
@@ -1,8 +1,18 @@
 from pytube import YouTube
 from sys import argv
 
-link = argv[1]
+link = input("Please enter a YouTube link: ")
 yt = YouTube(link)
 
 print("Title: ", yt.title)
-print(f"View: {yt.view}")
+print(f"View: {yt.views}")
+
+yd = yt.streams.get_highest_resolution()
+
+question = input("Do you want to download this video (yes/no)? ").lower()
+
+if question == "yes":
+    output = input("Where do you want to store this video? ")
+    yd.download(output)
+else:
+    quit()
